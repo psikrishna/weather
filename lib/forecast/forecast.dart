34666896b9bcfeee2aff55853589ec7faa9b5872
Sai Krishna Attaluri <attalurisaikrishna@gmail.com>
2020-05-31 03:17:31 +0530

    Commit#6
    Updating app acc to my layout

--- lib/forecast/forecast.dart
+++ lib/forecast/forecast.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weather/model/weather_forecast_model.dart';
 import 'package:weather/network/network.dart';
 import 'package:weather/ui/bottom_view.dart';
 import 'package:weather/ui/mid_view.dart';
+import 'package:weather/util/hex_color.dart';
 
 class weatherForecast extends StatefulWidget {
   @override
@@ -26,32 +28,80 @@ class _weatherForecastState extends State<weatherForecast> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0),
+        child: AppBar(
+          backgroundColor: HexColor("#4266A4"),
+        ),
+      ),
       body: ListView(
         children: <Widget>[
-          textFieldView(),
           Container(
-            child: FutureBuilder<WeatherForecastModel>(
-                future: forecastObject,
-                builder: (BuildContext context,
-                    AsyncSnapshot<WeatherForecastModel> snapshot) {
-                  if (snapshot.hasData) {
-                    return Column(
-                      children: <Widget>[
-                        midView(snapshot),
-                        bottomView(snapshot, context),
-                      ],
-                    );
-                  } else {
-                    return Center(
-                      child: Container(
-                        child: Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      ),
-                    );
-                  }
-                }),
-          )
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [HexColor("#4266A4"), HexColor("#CEADA7")],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 8.0,
+                ),
+                textFieldView(),
+                Container(
+                  child: FutureBuilder<WeatherForecastModel>(
+                      future: forecastObject,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<WeatherForecastModel> snapshot) {
+                        if (snapshot.hasData) {
+                          return Column(
+                            children: <Widget>[
+                              midView(snapshot),
+                              bottomView(snapshot, context),
+                            ],
+                          );
+                        } else {
+                          return Center(
+                            child: Container(
+                              child: Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                            ),
+                          );
+                        }
+                      }),
+                )
+              ],
+            ),
+          ),
+//          textFieldView(),
+//          Container(
+//            child: FutureBuilder<WeatherForecastModel>(
+//                future: forecastObject,
+//                builder: (BuildContext context,
+//                    AsyncSnapshot<WeatherForecastModel> snapshot) {
+//                  if (snapshot.hasData) {
+//                    return Column(
+//                      children: <Widget>[
+//                        midView(snapshot),
+//                        bottomView(snapshot, context),
+//                      ],
+//                    );
+//                  } else {
+//                    return Center(
+//                      child: Container(
+//                        child: Center(
+//                          child: CircularProgressIndicator(),
+//                        ),
+//                      ),
+//                    );
+//                  }
+//                }),
+//          )
         ],
       ),
     );
@@ -65,9 +115,17 @@ class _weatherForecastState extends State<weatherForecast> {
         child: TextField(
           decoration: InputDecoration(
             hintText: "Enter City Name",
-            prefixIcon: Icon(Icons.search),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
+            hintStyle: TextStyle(color: Colors.white),
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
             ),
             contentPadding: EdgeInsets.all(8.0),
           ),
